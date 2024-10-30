dbe8b0c13d8dfda295ab7968ac9b61f77f7276fd
Ollihj <ollihj@gmail.com>
2024-10-30 08:45:36 +0100

    123

--- src/gui/YatzyGui.java
+++ src/gui/YatzyGui.java
@@ -1,12 +1,30 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class YatzyGui extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Yatzy");
+        GridPane pane = new GridPane();
+        this.initContent(pane);
 
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void initContent(GridPane pane) {
+
+        pane.setGridLinesVisible(false);
+        pane.setPadding(new Insets(20));
+        pane.setHgap(10);
+        pane.setVgap(10);
     }
 }
